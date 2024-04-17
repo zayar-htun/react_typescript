b87784818456f71d7bc1900b7c9ca4ce4eba1397
“zayar <barblack2842@gmail.com>
2024-04-17 13:31:10 +0400

    Style Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Button from "./components/Button";
+import Container from "./components/Container";
 import Greet from "./components/Greet";
 import Heading from "./components/Heading";
 import Input from "./components/Input";
@@ -28,12 +29,15 @@ function App() {
             <Heading>PlaceHolder Text</Heading>
             <Oscar><Heading>Oscar winner - Dani Daniels</Heading></Oscar> */}
 
-            <Button
+            {/* <Button
                 handleClick={(event, id) => {
                     console.log("Button Click", event);
                 }}
             />
-            <Input value="" handleChange={event => console.log(event)} />
+            <Input value="" handleChange={event => console.log(event)} /> */}
+            <Container
+                styles={{ border: "1px solid black", padding: "1rem" }}
+            />
         </div>
     );
 }
---
+++ src/components/Container.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+type ContainerProps = {
+    styles : React.CSSProperties
+}
+export default function Container(props: ContainerProps) {
+    return (
+        <div style={props.styles}>
+            Text Content Goes Here
+        </div>
+    );
+}
